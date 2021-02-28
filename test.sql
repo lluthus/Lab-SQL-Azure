5ca6827b2a5b62d6d1a6e545a31202c9adc9c488
lluthus <lluthus@gmail.com>
2021-02-28 23:23:06 +0100

    .

--- test.sql
+++ test.sql
@@ -2,43 +2,43 @@
 
 DECLARE
    @id                  int = 101,
-	@codPurchaseReason   varchar(1) = 'V'
+	@c   varchar(1) = 'V'
 
 DECLARE @IDR TABLE (
    Id          INT,
-   JsonMapping varchar(max)
+   jsonfield varchar(max)
 ) 
 
 INSERT INTO @IDR VALUES (101,'{"Id":1,"Data":"2020-11-12T00:00:00","Totale":2440,"Codice":"A",
    "Dettagli":[
-      {"Codice":"A","Conto":"X","IVA":"0022","TaxableIncome":1000,"TaxAmount":220},
-      {"Codice":"A","Conto":"Y","IVA":"0022","TaxableIncome":1000,"TaxAmount":220}
+      {"Codice":"A","Conto":"X","Tax":"0022","TaxableIncome":1000,"TaxAmount":220},
+      {"Codice":"A","Conto":"Y","Tax":"0022","TaxableIncome":1000,"TaxAmount":220}
    ]}')
 
 DECLARE @T table(
-   IVA varchar(4) not null,
+   Tax varchar(4) not null,
    Consistent bit        not null
 )
 
 insert into @T    values('0022', 1)
 
 UPDATE @IDR
-set JsonMapping = JSON_MODIFY(
+set jsonfield = JSON_MODIFY(
 	--modifico il codice per ogni dettaglio
-	JSON_MODIFY(JsonMapping, '$.Dettagli'          , JSON_QUERY((Select 
+	JSON_MODIFY(jsonfield, '$.Dettagli'          , JSON_QUERY((Select 
 								 															 CONCAT('[', STRING_AGG(JSON_MODIFY(
-																															JSON_MODIFY([value], '$.Codice', @codPurchaseReason				 )
-																																					 , '$.IVA'   , IIF(tbpr.Consistent = 1, 
-																																											 JSON_VALUE([value], '$.IVA'),
+																															JSON_MODIFY([value], '$.Codice', @c				 )
+																																					 , '$.Tax'   , IIF(tbpr.Consistent = 1, 
+																																											 JSON_VALUE([value], '$.Tax'),
 																																															'')										  )                
 								 						 												 				  , ',') WITHIN GROUP (ORDER BY CAST([key] AS int)), ']')
 								 									            From  
-																						OPENJSON(JsonMapping, '$.Dettagli') detail
+																						OPENJSON(jsonfield, '$.Dettagli') detail
 																					Left Join
-																						@T			  tbpr on tbpr.IVA = JSON_VALUE(detail.[value], '$.IVA')
+																						@T			  tbpr on tbpr.Tax = JSON_VALUE(detail.[value], '$.Tax')
 																					)))
 	--modifico la causale di testata
-								  , '$.Codice', @codPurchaseReason)
+								  , '$.Codice', @c)
 From
 	 @IDR 
 Where
