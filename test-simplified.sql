7ae3f9ed6d2d0436fcbd745dfcc1956f5068eb00
Lluthus <lluthus@users.noreply.github.com>
2021-03-13 23:27:22 +0100

    Update test-simplified.sql
    .

--- test-simplified.sql
+++ test-simplified.sql
@@ -11,7 +11,7 @@ INSERT INTO @IDR VALUES (101,'{"Id":1,"Data":"2020-11-12T00:00:00","Totale":2440
       {"Codice":"A","Conto":"X","Tax":"0022","TaxableIncome":1000,"TaxAmount":220},
       {"Codice":"A","Conto":"Y","Tax":"0022","TaxableIncome":1000,"TaxAmount":220}
    ]}');
--- update all Dettagli.Codice
+-- update all Dettagli.Codice one time
 UPDATE @IDR
 set jsonfield = 
 		JSON_MODIFY(jsonfield, '$.Dettagli', JSON_QUERY((Select CONCAT('[', 
