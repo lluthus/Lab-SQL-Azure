f65d69e5fc4e840246d335b26b889121d215f93a
Lluthus <lluthus@users.noreply.github.com>
2021-03-15 22:32:01 +0100

    Update test-simplified.sql

--- test-simplified.sql
+++ test-simplified.sql
@@ -6,10 +6,10 @@ DECLARE @IDR TABLE (
    jsonfield varchar(max)
 );
 --init json
-INSERT INTO @IDR VALUES (101,'{"Id":1,"Data":"2020-11-12T00:00:00","Totale":2440,"Codice":"A",
+INSERT INTO @IDR VALUES (101,'{"Id":1,
    "Dettagli":[
-      {"Codice":"A","Conto":"X","Tax":"0022","TaxableIncome":1000,"TaxAmount":220},
-      {"Codice":"A","Conto":"Y","Tax":"0022","TaxableIncome":1000,"TaxAmount":220}
+      {"Codice":"A"},
+      {"Codice":"A"}
    ]}');
 -- update all Dettagli.Codice one time
 UPDATE @IDR
